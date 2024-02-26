6b2d59c73c8b30bffd975e5e30e880066b223e44
Wzs010429 <jkwzs0429@gmail.com>
2024-02-27 01:53:29 +1300

    20240226 update

--- 20240226/1.py
+++ 20240226/1.py
@@ -1,11 +1,13 @@
 # leetcode 1 两数之和 easy
 
+# 这道题就用哈希去做就好
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         # 哈希表去check target-X是否存在
         hashSet = dict()
 
+        # enumerate忘记了 他第一个返回坐标，第二个返回实际值
         # i是迭代值0123 j是nums对应i坐标下的值
         for i, j in enumerate(nums):
             if target - j in hashSet:
