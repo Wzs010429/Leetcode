fe4db32d3cd420acbf0c1760c535d91de0ce9500
Wzs010429 <jkwzs0429@gmail.com>
2024-03-06 17:21:54 +1300

    20240306 update

--- 20240306/226.py
+++ 20240306/226.py
@@ -70,6 +70,89 @@ class Solution:
 
         return root
 
+# 迭代法 中序遍历
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        stack = [root]
+
+        while stack:
+            tmp = stack.pop()
+            if tmp.left:
+                stack.append(tmp.left)
+            tmp.left, tmp.right = tmp.right, tmp.left
+            if tmp.left:
+                stack.append(tmp.left)
+
+        return root
+
+
+# 后序遍历 递归法
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        self.invertTree(root.right)
+        self.invertTree(root.left)
+
+        root.left, root.right = root.right, root.left
+
+        return root
+
+
+
+# 层序遍历 迭代法
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        queue = deque([root])
+
+        while queue:
+            tmp = queue.popleft()
+
+            if tmp.right:
+                queue.append(tmp.right)
+            tmp.left, tmp.right = tmp.right, tmp.left
+            if tmp.right:
+                queue.append(tmp.right)
+
+        return root
+
+
+
+
+
+
+
 
 
 
