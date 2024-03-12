071693d194a77f31cc51640c29db0950b08bb91c
Wzs010429 <jkwzs0429@gmail.com>
2024-03-12 22:31:06 +1300

    20240312 update

---
+++ 20240312/236.py
@@ -0,0 +1,29 @@
+# leetcode 236 二叉树的最近公共祖先
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == q or root == p:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if not left and not right:
+            return None
+
+        elif left and not right:
+            return left
+
+        elif not left and right:
+            return right
+
+        else:
+            return root
