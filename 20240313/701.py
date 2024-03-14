773c5bca1e67283a5439da837fb7b3ffc61548c7
Wzs010429 <jkwzs0429@gmail.com>
2024-03-15 03:54:41 +1300

    20240315 update

--- 20240313/701.py
+++ 20240313/701.py
@@ -77,43 +77,6 @@ class Solution:
         
 
 
-## 递归法
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-
-        if root.val == key:
-            if not root.right and not root.left:
-                return None
-
-            if not root.left:
-                return root.right
-
-            if not root.right:
-                return root.left
-
-            else:
-                tmp = root.right
-                while tmp.left:
-                    tmp = tmp.left
-                tmp.left = root.left
-                return root.right
-        
-        if root.val > key: 
-            root.left = self.deleteNode(root.left, key)
-        if root.val < key:
-            root.right = self.deleteNode(root.right, key)
-
-        return root
-
 
 
 
