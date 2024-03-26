db98cf595f165597ea232b49e40beec04771d9b2
Wzs010429 <jkwzs0429@gmail.com>
2024-03-27 11:22:41 +1300

    20240327 update

--- 20240327/56.py
+++ 20240327/56.py
@@ -1,2 +1,19 @@
 # leetcode 56 合并区间
 
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        result = []
+
+        intervals.sort(key=lambda x: x[0])
+
+        result.append(intervals[0])
+
+        for i in range(len(intervals) - 1):
+            if result[-1][1] >= intervals[i + 1][0]:
+                result[-1][1] = max(intervals[i + 1][1], result[-1][1])
+            else:
+                result.append(intervals[i + 1])
+
+        return result
+
